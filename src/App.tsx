d8afb846c227513bf2c8b5240f9db80089356901
clchen-arcadia <clchen.arcadia@gmail.com>
2023-03-17 12:21:13 -0400

    Revert "Clean up App.tsx and index.tsx"
    This reverts commit d54303ecf25a8b4c037e548380b512b255f5f4c6.

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,26 @@
 import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import NavBar from './NavBar';
-import Section from './Section';
-import HomeSection from './HomeSection';
-
-
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
-      <HomeSection />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
-
-
-
 
 export default App;
