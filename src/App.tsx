d54303ecf25a8b4c037e548380b512b255f5f4c6
clchen-arcadia <clchen.arcadia@gmail.com>
2023-03-17 12:19:43 -0400

    Clean up App.tsx and index.tsx
    Remove some stuff from default create-react-app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './NavBar';
+import Section from './Section';
+import HomeSection from './HomeSection';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <HomeSection />
     </div>
   );
 }
+
+
+
 
 export default App;
