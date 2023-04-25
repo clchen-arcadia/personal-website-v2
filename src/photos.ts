6cf6db3ab2ca4749286612e6737065ac1692a824
clchen-arcadia <clchen.arcadia@gmail.com>
2023-04-24 22:00:04 -0400

    Grab Warbler screenshots

---
+++ src/photos.ts
@@ -0,0 +1,32 @@
+import { IPhoto } from "./types";
+
+import warbler1 from "./images/warbler1.png";
+import warbler2 from "./images/warbler2.png";
+import warbler3 from "./images/warbler3.png";
+import warbler4 from "./images/warbler4.png";
+import warbler5 from "./images/warbler5.png";
+
+const photos: IPhoto[] = [
+  {
+    src: warbler1,
+    alt: `Warbler homepage`
+  },
+  {
+    src: warbler2,
+    alt: `Warbler homepage`
+  },
+  {
+    src: warbler3,
+    alt: `Warbler homepage`
+  },
+  {
+    src: warbler4,
+    alt: `Warbler homepage`
+  },
+  {
+    src: warbler5,
+    alt: `Warbler homepage`
+  },
+]
+
+export default photos;
