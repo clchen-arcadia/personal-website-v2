9912d116cf7ac4c47c4e61c98e65228abb7316dc
clchen-arcadia <clchen.arcadia@gmail.com>
2023-04-24 22:01:49 -0400

    Add goForward and goBack functionality
    Still need arrows to show

--- src/components/PhotoCarousel.tsx
+++ src/components/PhotoCarousel.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
-import { IPhotoCarousel } from "./types";
+import { IPhotoCarousel } from "../types";
 import "./PhotoCarousel.css";
-import PhotoCard from "./PhotoCard.tsx";
+import PhotoCard from "./PhotoCard";
 
 
 
@@ -10,9 +10,32 @@ function PhotoCarousel({ photos, title }: IPhotoCarousel) {
 
     const currCard = photos[currCardIdx];
 
+    function goBack(): void {
+        if (photos.length === 0) return;
+        if (currCardIdx === 0) setCurrCardIdx(photos.length - 1);
+        else setCurrCardIdx(currCardIdx - 1);
+    }
+
+    function goForward(): void {
+        if (photos.length === 0) return;
+        if (currCardIdx === photos.length - 1) setCurrCardIdx(0);
+        else setCurrCardIdx(currCardIdx + 1);
+    }
+
     return (
         <div>
-
+            <i
+                className={`bi bi-arrow-left-circle`}
+                onClick={goBack}
+            />
+            <PhotoCard
+                src={currCard.src}
+                alt={currCard.alt}
+            />
+            <i
+                className={`bi bi-arrow-right-circle`}
+                onClick={goForward}
+            />
         </div>
     );
 }
