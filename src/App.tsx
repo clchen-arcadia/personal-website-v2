919de08e9bb22088adb3de0eb6c89d403520d14a
clchen-arcadia <clchen.arcadia@gmail.com>
2023-04-24 22:00:34 -0400

    Put PhotoCarousel into App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import './App.css';
+import PhotoCarousel from './components/PhotoCarousel';
+import photos from './photos';
 
 
 function App() {
+  const carouselPhotos = photos;
+  const carouselTitle = "Flask Warbler"
+
   return (
     <div className="App">
       <h1>Hello World</h1>
+
+      <PhotoCarousel photos={carouselPhotos} title={carouselTitle} />
     </div>
   );
 }
