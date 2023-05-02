bcbcd77410ff7b0276b6e9e2cd468e2e60711c5c
clchen-arcadia <clchen.arcadia@gmail.com>
2023-05-02 00:54:11 -0400

    Add overflow pattern for image, image-container within PhotoCard component

--- src/components/PhotoCard.tsx
+++ src/components/PhotoCard.tsx
@@ -3,7 +3,7 @@ require("./PhotoCard.css");
 
 function PhotoCarousel({src, alt}: IPhoto) {
     return (
-    <div className="Card shadow border">
+    <div className="Card shadow border d-flex justify-content-center align-items-center">
         <img className="Card-image" src={src} alt={alt} />
     </div>
     )
