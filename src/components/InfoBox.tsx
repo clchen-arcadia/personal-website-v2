6be4e8e55df0d050b3f95854d0b26445b1883b97
clchen-arcadia <clchen.arcadia@gmail.com>
2023-05-02 11:43:13 -0400

    Add InfoBox component

---
+++ src/components/InfoBox.tsx
@@ -0,0 +1,14 @@
+import "./InfoBox.css";
+
+function InfoBox() {
+
+    return (
+        <>
+        <h4>Warbler</h4>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique sit veritatis reprehenderit consequuntur ab quam neque labore perspiciatis, sequi magnam in quaerat tenetur corrupti inventore, deleniti esse tempore facere dicta!</p>
+        </>
+    )
+}
+
+export default InfoBox;
