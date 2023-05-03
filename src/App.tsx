f6b58b159c440763728e7aa6bad1b38a31a6ade2
clchen-arcadia <clchen.arcadia@gmail.com>
2023-05-03 00:15:10 -0400

    Finish font overhaul on Intro, About, and Skills sections

--- src/App.tsx
+++ src/App.tsx
@@ -59,54 +59,54 @@ function App() {
         <section id="grid-logos">
 
           <section className="row">
-            <img className="col" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" alt="dev-icon-javascript"/>
+            <img className="col" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" alt="dev-icon-javascript" />
 
-            <img className="col" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg" alt="dev-icon-typescript"/>
+            <img className="col" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg" alt="dev-icon-typescript" />
 
-            <img className="col" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg" alt="dev-icon-python"/>
+            <img className="col" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg" alt="dev-icon-python" />
 
-            <img className="col" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg" alt="dev-icon-postgresql"/>
+            <img className="col" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg" alt="dev-icon-postgresql" />
           </section>
 
           <section className="row">
-            <img className="col" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/amazonwebservices/amazonwebservices-original.svg" alt="dev-icon-aws"/>
+            <img className="col" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/amazonwebservices/amazonwebservices-original.svg" alt="dev-icon-aws" />
 
-            <img className="col" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" alt="dev-icon-html"/>
+            <img className="col" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" alt="dev-icon-html" />
 
-            <img className="col" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" alt="dev-icon-css"/>
+            <img className="col" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" alt="dev-icon-css" />
 
-            <img className="col" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg" alt="dev-icon-bootstrap"/>
+            <img className="col" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg" alt="dev-icon-bootstrap" />
           </section>
 
           <section className="row">
-            <img className="col" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" alt="dev-icon-react"/>
+            <img className="col" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" alt="dev-icon-react" />
 
-            <img className="col" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg" alt="dev-icon-express"/>
+            <img className="col" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg" alt="dev-icon-express" />
 
-            <img className="col" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" alt="dev-icon-node"/>
+            <img className="col" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" alt="dev-icon-node" />
 
-            <img className="col" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/flask/flask-original.svg" alt="dev-icon-flask"/>
+            <img className="col" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/flask/flask-original.svg" alt="dev-icon-flask" />
           </section>
 
           <section className="row">
 
-            <img className="col" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jquery/jquery-original.svg" alt="dev-icon-jquery"/>
+            <img className="col" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jquery/jquery-original.svg" alt="dev-icon-jquery" />
 
-            <img className="col" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/django/django-plain.svg" alt="dev-icon-django"/>
+            <img className="col" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/django/django-plain.svg" alt="dev-icon-django" />
 
-            <img className="col" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg" alt="dev-icon-git"/>
+            <img className="col" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg" alt="dev-icon-git" />
 
-            <img className="col" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" alt="dev-icon-github"/>
+            <img className="col" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" alt="dev-icon-github" />
           </section>
 
           <section className="row">
-            <img className="col" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/npm/npm-original-wordmark.svg" alt="dev-icon-npm"/>
+            <img className="col" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/npm/npm-original-wordmark.svg" alt="dev-icon-npm" />
 
-            <img className="col" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sqlalchemy/sqlalchemy-original.svg" alt="dev-icon-sql-alchemy"/>
+            <img className="col" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sqlalchemy/sqlalchemy-original.svg" alt="dev-icon-sql-alchemy" />
 
-            <img className="col" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jest/jest-plain.svg" alt="dev-icon-jest"/>
+            <img className="col" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jest/jest-plain.svg" alt="dev-icon-jest" />
 
-            <img style={{ visibility: "hidden" }} className="col" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jest/jest-plain.svg" alt="dev-icon-jest"/>
+            <img style={{ visibility: "hidden" }} className="col" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jest/jest-plain.svg" alt="dev-icon-jest" />
           </section>
 
         </section>
@@ -118,28 +118,48 @@ function App() {
       >
         <h2>Projects</h2>
 
-        <div className="d-flex row">
-          <div className="col-sm">
-            <InfoBox
-            />
-          </div>
-
-          <div className="col-sm">
-            <PhotoCarousel
-              photos={carouselPhotos}
-              title={carouselTitle}
-            />
+        <div className="project" id="warbler-row">
+          <div className="d-flex row">
+            <div className="col-sm">
+              <div id="warbler-blurb">
+                <h4>Warbler</h4>
+                <p>
+                  {`A Twitter clone built in Flask Python, SQLAlchemy, and PostgreSQL`}
+                </p>
+              </div>
+            </div>
+            <div className="col-sm">
+              <PhotoCarousel
+                photos={carouselPhotos}
+                title={carouselTitle}
+              />
+            </div>
           </div>
         </div>
 
+        <div className="project" id="jobly-row">
+          <div className="d-flex row">
+            <div className="col-sm">
+              <div id="jobly-blurb">
+                <h4>Jobly</h4>
+                <p>
+                  {`A job board site built in React.js, Express.js, and PostgreSQL`}
+                </p>
+              </div>
+            </div>
+            <div className="col-sm">
+              <PhotoCarousel
+                photos={carouselPhotos}
+                title={carouselTitle}
+              />
+            </div>
+          </div>
+        </div>
 
       </section>
 
     </div>
   );
 }
-
-
-
 
 export default App;
