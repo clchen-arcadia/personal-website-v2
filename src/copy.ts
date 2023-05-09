42924311f8650d1fb057d261eca8790b37ea61ce
clchen-arcadia <clchen.arcadia@gmail.com>
2023-05-09 15:46:37 -0400

    Add line breaks

--- src/copy.ts
+++ src/copy.ts
@@ -2,7 +2,7 @@ export const projectTitleOne = `Asdf`
 
 export const projectBlurbOne = `asdf`;
 
-export const aboutMeBlurb = `Hi there! I’m Christopher Chen and I’m a full stack software engineer. I’ve recently graduated from Rithm School’s coding bootcamp and am working on networking, applying to jobs, and building out my portfolio and new projects!
+export const aboutMeBlurb = `Hi there! I’m Christopher Chen and I’m a full stack software engineer. I’ve recently graduated from Rithm School’s coding bootcamp and am working on networking, applying to jobs, and building out my portfolio and new projects! \n\n
 
-Fun facts: My favorite show is BoJack Horseman. My favorite book is A Tree Grows In Brooklyn. And I love hiking, board games, and cooking!
+Fun facts: My favorite show is BoJack Horseman. My favorite book is A Tree Grows In Brooklyn. And I love hiking, board games, and cooking! \n\n
 `
