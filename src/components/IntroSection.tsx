c0f14169faa8bd50034dcda5421f20207dfbfd95
clchen-arcadia <clchen.arcadia@gmail.com>
2023-05-29 23:49:41 -0400

    Add styling on background image and container

--- src/components/IntroSection.tsx
+++ src/components/IntroSection.tsx
@@ -1,15 +1,26 @@
 import './IntroSection.css';
-import backgroundPhoto from "../images/background-high.jpg";
+import backgroundPhoto from "../images/cherry-blossom-nyc-2.jpg";
+
+/**
+ * Renders the IntroSection component.
+ *
+ * App --> IntroSection
+ *
+ * Props: height, width
+ */
 
 function IntroSection() {
+
     return (
         <section
             id="intro"
+            className="d-flex justify-content-center align-items-center"
         >
-            <img src={backgroundPhoto} alt="background with cherry blossoms" id="background" />
-            <h1>Hello World, I'm Christopher Chen</h1>
-            <h2 id="h2-1">Software Engineer</h2>
-            <h2 id="h2-2">New York, NY</h2>
+            <img src={backgroundPhoto} alt="background with cherry blossoms" id="background"
+            />
+            <h1 className="font-link" >Hello World, I'm Christopher Chen</h1>
+            <h2 className="font-link" id="h2-1">Software Engineer<br />New York, NY</h2>
+            {/* <h2 className="font-link" id="h2-2">New York, NY</h2> */}
         </section>
     );
 }
