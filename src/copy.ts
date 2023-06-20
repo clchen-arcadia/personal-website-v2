e412a42a6700406e80fb830817041e17c42dd6bc
Christopher Chen <clchen.arcadia@gmail.com>
2023-06-19 19:12:24 -0700

    Edit about me blurb

--- src/copy.ts
+++ src/copy.ts
@@ -2,7 +2,8 @@ export const projectTitleOne = `Asdf`
 
 export const projectBlurbOne = `asdf`;
 
-export const aboutMeBlurb = `Hi there! I’m Christopher Chen and I’m a full stack software engineer. I’ve recently graduated from Rithm School’s coding bootcamp and am working on networking, applying to jobs, and building out my portfolio and new projects! \n\n
+export const aboutMeBlurb = `Hi there! I’m Christopher Chen and I’m a full stack software engineer. I’ve recently graduated from Rithm School’s web dev coding bootcamp and am working on networking, applying to jobs, and building out my portfolio and new projects!
 
-Fun facts: My favorite show is BoJack Horseman. My favorite book is A Tree Grows In Brooklyn. And I love hiking, board games, and cooking! \n\n
-`
+Fun facts: My favorite show is BoJack Horseman. My favorite book is A Tree Grows In Brooklyn. And I love hiking, board games, and cooking!
+
+Reach out if you want to ask me anything about my projects, career change, or chat about my hobbies!`
