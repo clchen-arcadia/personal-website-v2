e2b059801846560801e82dc5b58bfc0bdcc46a7f
Christopher Chen <clchen.arcadia@gmail.com>
2023-06-20 20:20:41 -0700

    Add NavBar custom component

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import IntroSection from './components/IntroSection';
 import AboutSection from './components/AboutSection';
 import SkillSection from './components/SkillSection';
 import ProjectSection from './components/ProjectSection';
+import NavBar from './components/NavBar';
 
 function App() {
-
   return (
     <div id="App">
+
+      <NavBar />
 
       <IntroSection />
 
