e3f7ed78e37c636d48e61f6be9b6dead2ee4ad7b
Christopher Chen <clchen.arcadia@gmail.com>
2023-06-20 23:47:40 -0700

    Remove typing

--- src/components/PhotoCard.tsx
+++ src/components/PhotoCard.tsx
@@ -1,7 +1,7 @@
 import { IPhoto } from "../types";
 import "./PhotoCard.css";
 
-function PhotoCarousel({src, alt}: IPhoto) {
+function PhotoCarousel({src, alt}) {
     return (
     <div className="Card shadow border d-flex justify-content-center align-items-center">
         <img className="Card-image" src={src} alt={alt} />
