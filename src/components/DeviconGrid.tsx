7aae9bb58346fea7cb05855cecb3415cb2a23f87
Christopher Chen <clchen.arcadia@gmail.com>
2023-06-20 20:24:31 -0700

    Implement new pattern with DeviconGrid custom component

---
+++ src/components/DeviconGrid.tsx
@@ -0,0 +1,81 @@
+import "./DeviconGrid.css";
+import DeviconCard from "./DeviconCard";
+
+function DeviconGrid({ gridWidth, gridHeight, numRows, numColumns, deviconTuples }) {
+    const cellWidth = gridWidth / (numColumns + 2);
+    const cellHeight = gridHeight / (numRows + 1);
+
+    const rowsToRender = [];
+    for (let i = 0; i < numRows; i++) {
+        rowsToRender.push(
+            deviconTuples.slice(numColumns * i, numColumns * i + numColumns)
+        );
+    }
+
+    return (
+        <div
+            className="skillGrid"
+            style={{
+                border: "1px solid red"
+            }}
+        >
+            {
+                rowsToRender.map(
+                    r => {
+                        return (
+                            <div className="skillRow"
+                            >
+                                {
+                                    r.map(
+                                        t => {
+                                            return (
+                                                <DeviconCard
+                                                    width={`${cellWidth}vw`}
+                                                    height={`auto`}
+                                                    source={t[0]}
+                                                    alt={t[1]}
+                                                    title={t[2]}
+                                                />
+                                            );
+                                        }
+                                    )
+                                }
+                            </div>
+                        );
+                    }
+                )
+            }
+        </div>
+    );
+
+}
+
+export default DeviconGrid;
+
+
+/**
+ *
+        {
+            for(let iRow = 0; iRow <rows; iRow++) {
+        for (let iCol = 0; iCol < columns; iCol++) {
+            deviconTuples
+                .slice(columns * iRow, columns * iRow + columns)
+                .map(
+                    t => {
+                        return (
+                            <DeviconCard
+                                width={cellWidth}
+                                height={cellHeight}
+                                source={t[0]}
+                                alt={t[1]}
+                                title={t[2]}
+                            />
+                        );
+                    }
+                );
+        }
+    }
+}
+
+width, height, rows, columns, deviconTuples
+ */
